1fa2377715c768729f1e464bf0744fb060a272d3
ruchita-iyer <iyer.ru@northeastern.edu>
2023-07-29 18:46:15 -0400

    task 6 done

--- final_project/index.js
+++ final_project/index.js
@@ -3,20 +3,33 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
 const app = express();
-
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    if(req.session.authorization) {
+        token = req.session.authorization['token'];
+     // comparing the token with the one generated in the log in with the token present in the session
+        jwt.verify(token, "secret_key",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated", error: err})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
+
 });
  
 const PORT =5000;
 
-app.use("/customer", customer_routes);
+app.use("/customer", customer_routes); //from the auth_users.js
 app.use("/", genl_routes);
-
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running on port ", PORT));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,26 +3,48 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{ "username": "ibm", "password": "ibm123"}];
+//let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  let isUsernameValid = false
+  users.map(user => {
+    if (user.username == username) {
+      isUsernameValid = true
+    }
+  })
+  return isUsernameValid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  let isUserValid = false
+  users.map(user => {
+    if (user.username == username && user.password == password) {
+      isUserValid = true
+    }
+  })
+  return isUserValid
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body
+  let isUserAuthorized = authenticatedUser(username, password)
+  let message = ""
+  if (isUserAuthorized)
+    message = "User login success!"
+  else
+    message = "User login failed!"
+  return res.status(300).json({ message: message });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params
+  const { review } = req.query
+  const book = books[isbn]
+  book.reviews = { review: review }
+  return res.status(300).json({ message: "Review updated" });
 });
 
 module.exports.authenticated = regd_users;
